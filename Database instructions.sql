2399e371f3dd43b2067b020b7b9d83b3fb55e1e9
juicyj201 <joshuajulies06@gmail.com>
2024-06-14 12:37:35 +0200

    Added data using SQLite

---
+++ Database instructions.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE TicketTracking; 
+
+CREATE TABLE Users (E
+    MP_ID INTEGER PRIMARY KEY, 
+    Name varchar(50), 
+    Emp_Type varchar(20)
+); 
+
+CREATE TABLE Tickets (Ticket_ID INTEGER PRIMARY KEY,
+    Summary varchar(300) NOT NULL,
+    Description varchar(500) NOT NULL,
+    Ticket_Type varhcar(50) NOT NULL,
+    Severity varchar(50),
+    Priority varchar(20)
+); 
